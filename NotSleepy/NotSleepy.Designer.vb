d04d2485b18551eb1b748025a4968680a1dc44a4
davphestark <davphestark@gmail.com>
2016-12-15 20:57:26 -0500

    initial add

---
+++ NotSleepy/NotSleepy.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class NotSleepy
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(NotSleepy))
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.RunningToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.StopToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RunningToolStripMenuItem, Me.StopToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(99, 70)
+        '
+        'RunningToolStripMenuItem
+        '
+        Me.RunningToolStripMenuItem.Name = "RunningToolStripMenuItem"
+        Me.RunningToolStripMenuItem.Size = New System.Drawing.Size(98, 22)
+        Me.RunningToolStripMenuItem.Text = "Start"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(98, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotSleepy"
+        Me.NotifyIcon1.Visible = True
+        '
+        'StopToolStripMenuItem
+        '
+        Me.StopToolStripMenuItem.Name = "StopToolStripMenuItem"
+        Me.StopToolStripMenuItem.Size = New System.Drawing.Size(98, 22)
+        Me.StopToolStripMenuItem.Text = "Stop"
+        '
+        'NotSleepy
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "NotSleepy"
+        Me.Opacity = 0.0R
+        Me.ShowInTaskbar = False
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents RunningToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents StopToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ NotSleepy/NotSleepy.vb
@@ -0,0 +1,70 @@
+﻿Public Class NotSleepy
+    Private Delegate Sub ShakeItDelegate(ByVal iPix As Integer)
+    Private QueueProcessorThread As Threading.Thread
+    Private QueueProcessorCancel As Boolean
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        StopQueueProcessor()
+        Me.Close()
+    End Sub
+
+    Private Sub RunningToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RunningToolStripMenuItem.Click
+        StartQueueProcessor()
+    End Sub
+
+    Private Sub NotSleepy_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+        End If
+    End Sub
+
+    Private Sub StartQueueProcessor()
+        Debug.WriteLine("Starting Queue Processor...")
+        QueueProcessorCancel = False
+        QueueProcessorThread = New Threading.Thread(AddressOf QueueProcessor)
+        QueueProcessorThread.IsBackground = True
+        QueueProcessorThread.Start()
+    End Sub
+
+    Private Sub StopQueueProcessor()
+        If QueueProcessorThread IsNot Nothing AndAlso QueueProcessorThread.IsAlive Then
+            Debug.WriteLine("Stopping Queue Processor...")
+            QueueProcessorCancel = True
+            QueueProcessorThread.Interrupt()
+            If Not QueueProcessorThread.Join(1000) Then
+                Debug.WriteLine("Queue Processor did not respond to stop request. Aborting Queue Processor...")
+                QueueProcessorThread.Abort()
+            End If
+        End If
+    End Sub
+
+    Private Sub QueueProcessor()
+        Debug.WriteLine("Queue Processor Started:" & Now.ToString)
+        Try
+            Do
+                Try
+                    Debug.WriteLine("Processing Queue...")
+                    Invoke(New ShakeItDelegate(AddressOf ShakeIt), New Object() {1})
+                    Threading.Thread.Sleep(480000)
+                Catch ex As Threading.ThreadInterruptedException
+                End Try
+            Loop Until QueueProcessorCancel
+        Catch ex As Threading.ThreadAbortException
+            Debug.WriteLine("Queue Processor Aborted.")
+        Catch ex As Exception
+            Debug.WriteLine(String.Format("An unhandled error occured: {0}", ex.Message))
+        End Try
+        Debug.WriteLine("Queue Processor Stopped.")
+    End Sub
+
+    Private Sub ShakeIt(ByVal iPix As Integer)
+        'Me.Cursor = New Cursor(Cursor.Current.Handle)
+        'Cursor.Position = New Point(Cursor.Position.X - iPix, Cursor.Position.Y)
+        'Cursor.Position = New Point(Cursor.Position.X + iPix, Cursor.Position.Y)
+        SendKeys.Send("{NUMLOCK}{NUMLOCK}")
+    End Sub
+
+    Private Sub StopToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StopToolStripMenuItem.Click
+        StopQueueProcessor()
+    End Sub
+End Class
